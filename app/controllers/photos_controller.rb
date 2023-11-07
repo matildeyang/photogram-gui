97d55158a2f13a490fcda8af5f472da12eaa7538
matildeyang <91755817+matildeyang@users.noreply.github.com>
2023-11-07 20:46:58 +0000

    comments

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -4,4 +4,14 @@ class PhotosController < ApplicationController
     @list_of_photos = matching_photos.order({ :created_at => :desc})
     render({ :template => "photo_templates/index"})
   end 
+
+  def show 
+    photo_id = params.fetch("path_id")
+
+    matching_photos = Photo.where({ :id => photo_id})
+
+    @the_photo = matching_photos.at(0)
+    
+    render({ :template => "photo_templates/show"})
+  end 
 end 
