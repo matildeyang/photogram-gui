733e521e7e2521ff01073821dceb829ff834550d
matildeyang <91755817+matildeyang@users.noreply.github.com>
2023-11-07 23:40:58 +0000

    issue fix

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get("/", { :controller => "users", :action => "index"})
+
   get("/users", { :controller => "users", :action => "index"})
   get("/users/:path_username", { :controller => "users", :action => "show"})
 
